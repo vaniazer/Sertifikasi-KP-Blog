3260e647b63897ccd5118998606e4d8ebb7e65d1
VANIA ZERLINDA <vaniazerlinda19@gmail.com>
2021-02-22 09:28:09 +0700

    sertifikasi

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci3kp/';
+$config['base_url'] = 'https://dashboard.heroku.com/apps/blogku-resep/';
 
 /*
 |--------------------------------------------------------------------------
